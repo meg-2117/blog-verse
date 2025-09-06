21d332c7c055ee37d6ba6896ce7c17a1c38888f9
meg-2117 <meghanabiddika3@gmail.com>
2025-09-06 12:33:32 +0530

    learned data types

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log("Hello World, Meghana")
+
+let name = "dev astra"
+console.log(name)
+
+let num = 18
+console.log(num)
+
+let isLogged=false
+console.log(isLogged)
+
+let x
+console.log(x)
+
+let y=null
+console.log(y)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person ={
+    name :"meg",
+    age : 19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+console.log(typeof y)
